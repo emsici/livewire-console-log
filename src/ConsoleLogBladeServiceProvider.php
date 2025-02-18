2252fdfbd6f6c1b666dd282604b1b3875ff3837f
Alin <alin@ar-cyber-lab.ro>
2025-02-18 20:57:18 +0200

    modified composer.json

---
+++ src/ConsoleLogBladeServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ARCyberLab\ConsoleLog;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class ConsoleLogBladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('livewireConsoleLogs', function () {
+            return "<?php echo view('console-log::scripts'); ?>";
+        });
+
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'console-log');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
