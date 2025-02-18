084dc9093a0049571d6f234c90ad885505f9f27a
Alin <alin@ar-cyber-lab.ro>
2025-02-18 20:45:21 +0200

    modified composer.json

--- src/ConsoleLog.php
+++ src/ConsoleLog.php
@@ -11,7 +11,7 @@ class ConsoleLog extends Component
         foreach ($messages as $message) {
             $msg = $message[0];
             $css = isset($message[1]) ? $message[1] : '';
-            self::dispatchBrowserEvent('consoleLog', [['application' => 'MyApp', 'version' => '1.0', 'module' => $msg, 'style' => $css]]);
+            self::emit('consoleLog', [['application' => 'MyApp', 'version' => '1.0', 'module' => $msg, 'style' => $css]]);
         }
     }
 }
